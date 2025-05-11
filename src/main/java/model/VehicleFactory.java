f7a0558de58bbc624f72ac202d8b583ddf34e64e
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-05-11 14:40:00 -0400

    Deleted unnecessary lines of code

--- src/main/java/model/VehicleFactory.java
+++ src/main/java/model/VehicleFactory.java
@@ -8,9 +8,9 @@ public class VehicleFactory {
             case COMPACT : return new CompactVehicle(licenseNumber,ownerName);
             case REGULAR: return new RegularVehicle(licenseNumber,ownerName);
             case LARGE: return new LargeVehicle(licenseNumber,ownerName);
+            default:
+                throw new IllegalArgumentException("Invalid Spot Type");
         }
-
-        return  null;
 
     }
 
